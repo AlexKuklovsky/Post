6fab7014df4de8b5c5ffa6cbd6b4cf3bbcb978aa
Alex Kuklolvsky <potatoharmles@gmail.com>
2025-12-03 15:00:27 +0100

    obrazky

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ from flask import Flask, request , render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home ():
-    if request.method == 'POST':
-        data = request.form["data"]
-        return f"Method POST: {data}"
 
-    return render_template("index.html")
+images = [
+    "image_01", 
+    "image_02", 
+    "image_11", 
+    "image_12", 
+    "image_15", 
+    "image_27", 
+    "image_37"]
+@app.route('/', methods=['GET'])
+def home_get():
+    # print(request.args.get("img"))
+    image=request.args.get("img")
+    if image in images:
+        image = "images/" + image + ".jpg"
+    else:
+        image = None
+    return render_template("index.html", images=images, image=image)
+
+@app.route('/', methods=['POST'])
+def home_post():
+    data = request.form["data"]
+    return f"Method POST: {data}"
